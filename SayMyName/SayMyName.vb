3016cf3535e5cb189511748156f44a6d43908b03
Victoria Gomez <77286079+ToriGomez@users.noreply.github.com>
2021-01-21 18:36:04 -0700

    Add project files.

---
+++ SayMyName/SayMyName.vb
@@ -0,0 +1,23 @@
+﻿'Tori Gomez
+'RCET0265
+'Spring 2021
+'Say My Name
+'
+
+Option Explicit On
+Option Strict On
+
+Module SayMyName
+
+    Sub Main()
+        Dim userInput As String
+
+        Console.WriteLine("Type Name and Press Enter")
+
+        userInput = Console.ReadLine()
+        Console.WriteLine($"Welcome! Thank you {userInput}!")
+
+        Console.ReadLine()
+    End Sub
+
+End Module
